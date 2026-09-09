cbb8cdeeb9ea92d8b1dda006de7ee515f265d922
ysh0305 <s1hung@ucsd.edu>
2026-09-09 07:58:36 -0700

    fix jest setup path for hooks

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,5 @@
 // Jest config for the Next.js frontend.
+const path = require("node:path");
 const nextJest = require("next/jest");
 
 const createJestConfig = (nextJest.default || nextJest)({ dir: "./" });
@@ -6,7 +7,7 @@ const createJestConfig = (nextJest.default || nextJest)({ dir: "./" });
 /** @type {import('jest').Config} */
 const customJestConfig = {
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  setupFilesAfterEnv: [path.join(__dirname, "jest.setup.ts")],
   collectCoverage: true,
   collectCoverageFrom: [
     "app/**/*.{ts,tsx}",
