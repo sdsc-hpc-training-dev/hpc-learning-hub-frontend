6941587dbc62f1d89f92031b358adec9d8262cc8
Keyura Valalla <ke7nv21@gmail.com>
2026-09-16 09:10:50 -0700

    eslint fix

--- components/__tests__/Navbar.test.tsx
+++ components/__tests__/Navbar.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, /*fireEvent*/ } from "@testing-library/react";
 import { usePathname } from "next/navigation";
 import Navbar from "../Navbar";
 
